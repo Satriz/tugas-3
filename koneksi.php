09278013adb792e4db6b41bf00a4520d83cc2a07
Satriz <48471649+Satriz@users.noreply.github.com>
2019-03-26 15:51:12 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-    $kon = mysqli_connect("localhost","id8951292_tugas3","gatot@123","id8951292_tugas3");
+    $kon = mysqli_connect("localhost","id8951324_sate","12345","id8951324_tugas");
     if (mysqli_connect_errno()) {
         echo "Koneksi database gagal : " .mysqli_connect_error();
     }
